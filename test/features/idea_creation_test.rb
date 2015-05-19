ca0cd16e1fe2f12d9c0293a1a7a6481cd5063472
Edward McCarthy <efmccarthy26@gmail.com>
2015-05-18 18:56:27 -0600

    added a couple more tests

--- test/features/idea_creation_test.rb
+++ test/features/idea_creation_test.rb
@@ -8,6 +8,15 @@ class IdeaCreationTest < ActionDispatch::IntegrationTest
     within(".banner") do
       assert page.has_content?("Welcome to IdeaBox - The Sequel!")
     end
+    within(".collection") do
+      assert page.has_content?("Your Bright Ideas")
+    end
+  end
+
+  test "user can create an idea" do
+    visit root_path
+    click_link_or_button("Add New Idea")
+    
   end
 
 end
